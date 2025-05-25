467a6ae40a6103f5a047abc17ef18d25733b04dd
Arendrast <120911943+Arendrast@users.noreply.github.com>
2025-05-25 22:28:59 +0300

    Added clients connection tracking for server and renamed for client

---
+++ Runtime/Scripts/Server/ClientsConnectionTrackingServiceForServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FishNet.Connection;
+using FishNet.Transporting;
+
+namespace MainHub.Server.Scripts.Infrastructure.Services
+{
+    public class ClientsConnectionTrackingServiceForServer
+    {
+        public event Action<NetworkConnection> Disconnected, Connected;
+
+        public void TryInvokeActionByConnectionState(NetworkConnection conn, RemoteConnectionStateArgs args)
+        {
+            if (args.ConnectionId == -1)
+                return;
+            
+            if (args.ConnectionState == RemoteConnectionState.Started)
+                Connected?.Invoke(conn);
+            else
+                Disconnected?.Invoke(conn);
+        }
+    }
+}
