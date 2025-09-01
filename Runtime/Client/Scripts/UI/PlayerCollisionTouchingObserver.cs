2deea2d0954bc914651a8d01235a4f1fcfa1bc1e
Arendrast <120911943+Arendrast@users.noreply.github.com>
2025-09-01 19:33:47 +0300

    updated schema and added touching observer dispose

--- Runtime/Client/Scripts/UI/PlayerCollisionTouchingObserver.cs
+++ Runtime/Client/Scripts/UI/PlayerCollisionTouchingObserver.cs
@@ -9,20 +9,28 @@ namespace WelwiseSharedModule.Runtime.Client.Scripts.UI
         public event Action<RaycastHit> Touched;
 
         private readonly PhysicsScene _physicsScene;
+        private readonly bool _shouldCheckOnlyGetKeyDown;
 
+        private event Action _disposed;
+        
         public PlayerCollisionTouchingObserver(MonoBehaviourObserver monoBehaviourObserver, Camera camera,
-            float rayLength, LayerMask mask, PhysicsScene physicsScene)
+            float rayLength, LayerMask mask, PhysicsScene physicsScene, bool shouldCheckOnlyGetKeyDown)
         {
             _physicsScene = physicsScene;
+            _shouldCheckOnlyGetKeyDown = shouldCheckOnlyGetKeyDown;
             void RayCast() => TryRayCastAndInvokeAction(camera, rayLength, mask);
-
+            void UnsubscribeRaycast() => monoBehaviourObserver.Updated -= RayCast; 
+            
             monoBehaviourObserver.Updated += RayCast;
+            _disposed += UnsubscribeRaycast;
         }
+
+        public void Dispose() => _disposed?.Invoke();
 
         private void TryRayCastAndInvokeAction(Camera camera, float rayLength, LayerMask layerMask)
         {
-            if (!Input.GetKey(KeyCode.Mouse0) && !Input.GetKeyDown(KeyCode.Mouse0) && 
-                !Input.GetKeyUp(KeyCode.Mouse0) || UITools.IsInteractWithUI())
+            if (!Input.GetKeyDown(KeyCode.Mouse0) && (_shouldCheckOnlyGetKeyDown || (!Input.GetKey(KeyCode.Mouse0) && 
+                !Input.GetKeyUp(KeyCode.Mouse0))) || UITools.IsInteractWithUI())
                 return;
 
             var ray = camera.ScreenPointToRay(Input.mousePosition);
